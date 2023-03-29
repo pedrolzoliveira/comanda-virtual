7c1928af36af64f61e317935d8e555bcf566ccf4
Pedro Oliveira <pluiz@mychirotouch.com>
2023-03-29 00:30:26 -0300

    change comanda value when a paymnet  is added

--- src/comandas/use-cases/add-payment.test.ts
+++ src/comandas/use-cases/add-payment.test.ts
@@ -1,23 +1,31 @@
+import { prismaClient } from '@/config/prisma/client'
 import { factory } from '@/utils/test/factory'
 import { faker } from '@faker-js/faker'
+import { type Comanda, type Transaction } from '@prisma/client'
 import { addPayment } from './add-payment'
 
 describe('addPayment', () => {
   let comandaId: string
   beforeAll(async () => {
-    ({ id: comandaId } = await factory.createComanda())
+    ({ id: comandaId } = await factory.createComanda({ amount: 1500 }))
   })
 
   describe('adds payment to comanda', () => {
-    it('creates a transaction of type payment', async () => {
-      const PAYMENT_DATA = {
-        comandaId,
-        description: faker.random.words(5),
-        value: 1000
-      }
-      await expect(
-        addPayment(PAYMENT_DATA)
-      ).resolves.toEqual({
+    let comanda: Comanda
+    let payment: Transaction
+
+    const PAYMENT_DATA = {
+      description: faker.random.words(5),
+      value: 1000
+    }
+
+    beforeAll(async () => {
+      payment = await addPayment({ ...PAYMENT_DATA, comandaId })
+      comanda = await prismaClient.comanda.findUnique({ where: { id: comandaId } }) as Comanda
+    })
+
+    it('creates a transaction of type payment', () => {
+      expect(payment).toEqual({
         id: expect.any(String),
         comandaId,
         description: PAYMENT_DATA.description,
@@ -25,6 +33,10 @@ describe('addPayment', () => {
         type: 'payment',
         createdAt: expect.any(Date)
       })
+    })
+
+    it('removes it values from the comanda', () => {
+      expect(comanda.amount).toBe(500)
     })
   })
 })
--- src/comandas/use-cases/add-payment.ts
+++ src/comandas/use-cases/add-payment.ts
@@ -7,16 +7,23 @@ interface AddPaymentParams {
 }
 
 export const addPayment = async (data: AddPaymentParams) => {
-  return await prismaClient.transaction.create({
-    data: {
-      amount: data.value,
-      description: data.description,
-      type: 'payment',
-      comanda: {
-        connect: {
-          id: data.comandaId
+  return await prismaClient.$transaction(async tx => {
+    const comanda = await tx.comanda.findUnique({ where: { id: data.comandaId } })
+    if (!comanda) throw new Error('Comanda not found')
+
+    await tx.comanda.update({ data: { amount: comanda.amount - data.value }, where: { id: data.comandaId } })
+
+    return await prismaClient.transaction.create({
+      data: {
+        amount: data.value,
+        description: data.description,
+        type: 'payment',
+        comanda: {
+          connect: {
+            id: data.comandaId
+          }
         }
       }
-    }
+    })
   })
 }
